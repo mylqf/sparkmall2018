a4f5fe2a87218673843ad80fa970b8764f601510
liao <1234>
2019-03-25 17:58:39 +0800

    test1

--- src/main/java/JUC/SaleTicketDemo01.java
+++ src/main/java/JUC/SaleTicketDemo01.java
@@ -1,17 +1,31 @@
 package JUC;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class SaleTicketDemo01  {
 
-
+   static int ticketNum=30;
 
     public static void main(String[] args) {
 
-        new Saler().start();
-        new Saler().start();
-        new Saler().start();
+//        new Saler().start();
+//        new Saler().start();
+//        new Saler().start();
 
+        new Thread(()->{
+            while (ticketNum>=1) {
+            ticketNum -= 1;
+            System.out.println(Thread.currentThread().getName()+" "+"还剩" + ticketNum + "张票");
+
+        }}, "a");
+
+        new Thread(()->{
+            while (ticketNum>=1) {
+                ticketNum -= 1;
+                System.out.println(Thread.currentThread().getName()+" "+"还剩" + ticketNum + "张票");
+
+            }}, "b");
     }
 
 }
@@ -21,15 +35,16 @@ public class SaleTicketDemo01  {
 
 class Saler extends Thread{
     //ArrayList
+    ///HashSet
 
-    int ticketNum=30;
-
-    @Override
-    public   void run() {
-        while (ticketNum>=1) {
-            ticketNum -= 1;
-            System.out.println(Thread.currentThread().getName()+" "+"还剩" + ticketNum + "张票");
-
-        }
-    }
+//    int ticketNum=30;
+//
+//    @Override
+//    public   void run() {
+//        while (ticketNum>=1) {
+//            ticketNum -= 1;
+//            System.out.println(Thread.currentThread().getName()+" "+"还剩" + ticketNum + "张票");
+//
+//        }
+//    }
 }
---
+++ src/main/java/JUC/WaitNotyDemo.java
@@ -0,0 +1,7 @@
+package JUC;
+
+public class WaitNotyDemo {
+
+
+
+}
